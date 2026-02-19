dfa4ea685bd741c402bb2b3127e6bff41226b61a
mrJay090903 <jac2022-2256-23916@bicol-u.edu.ph>
2026-02-19 09:05:17 +0800

    new

--- migrations/009_fix_votes_multi_select.sql
+++ migrations/009_fix_votes_multi_select.sql
@@ -2,43 +2,37 @@
 -- Date: 2026-02-19
 -- Issue: Unique constraint on (election_id, student_id, position_id) prevents
 --        voting for 2 candidates on representative positions where max_votes = 2
-
 -- Drop the unique constraint that prevents multiple votes per position
 -- The constraint may be named differently depending on how it was created
-
 -- Try dropping by common constraint names
-DO $$
+DO $$ BEGIN -- Drop unique index if it exists
+DROP INDEX IF EXISTS votes_election_student_position_unique;
+DROP INDEX IF EXISTS idx_votes_unique;
+DROP INDEX IF EXISTS votes_unique_vote;
+DROP INDEX IF EXISTS unique_vote;
+DROP INDEX IF EXISTS votes_election_id_student_id_position_id_key;
+-- Try to drop named constraint
 BEGIN
-  -- Drop unique index if it exists
-  DROP INDEX IF EXISTS votes_election_student_position_unique;
-  DROP INDEX IF EXISTS idx_votes_unique;
-  DROP INDEX IF EXISTS votes_unique_vote;
-  DROP INDEX IF EXISTS unique_vote;
-  DROP INDEX IF EXISTS votes_election_id_student_id_position_id_key;
-  
-  -- Try to drop named constraint
-  BEGIN
-    ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_election_student_position_unique;
-  EXCEPTION WHEN undefined_object THEN NULL;
-  END;
-  
-  BEGIN
-    ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_election_id_student_id_position_id_key;
-  EXCEPTION WHEN undefined_object THEN NULL;
-  END;
-  
-  BEGIN
-    ALTER TABLE votes DROP CONSTRAINT IF EXISTS unique_vote;
-  EXCEPTION WHEN undefined_object THEN NULL;
-  END;
-  
-  BEGIN
-    ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_unique_vote;
-  EXCEPTION WHEN undefined_object THEN NULL;
-  END;
+ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_election_student_position_unique;
+EXCEPTION
+WHEN undefined_object THEN NULL;
+END;
+BEGIN
+ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_election_id_student_id_position_id_key;
+EXCEPTION
+WHEN undefined_object THEN NULL;
+END;
+BEGIN
+ALTER TABLE votes DROP CONSTRAINT IF EXISTS unique_vote;
+EXCEPTION
+WHEN undefined_object THEN NULL;
+END;
+BEGIN
+ALTER TABLE votes DROP CONSTRAINT IF EXISTS votes_unique_vote;
+EXCEPTION
+WHEN undefined_object THEN NULL;
+END;
 END $$;
-
 -- Add a new unique constraint that allows multiple votes per position
 -- but prevents voting for the same candidate twice in the same election
-CREATE UNIQUE INDEX IF NOT EXISTS votes_election_student_candidate_unique
-ON votes (election_id, student_id, candidate_id);
+CREATE UNIQUE INDEX IF NOT EXISTS votes_election_student_candidate_unique ON votes (election_id, student_id, candidate_id);
